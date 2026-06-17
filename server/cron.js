ba5b295ac008549ad0eca13d6309c32a85e2edee
Gaurang Saini <gaurangsaini01@gmail.com>
2026-06-17 13:36:02 +0530

    Added FastApi Service To Cron

--- server/cron.js
+++ server/cron.js
@@ -2,8 +2,18 @@ const cron = require('cron');
 const https = require('https');
 
 const url= `https://studynotion-zfck.onrender.com/api/v1/course/getallcategories`;
+const url2= `https://studynotion-ai-backend.onrender.com`;
 const job = new cron.CronJob('*/12 * * * *',function(){
     https.get(url,(res)=>{
+        if(res.statusCode === 200){
+            console.log('Server Restarted');
+        }
+        else{
+            console.error('Failed to start server');
+        }
+    }).on('error',(err)=>{
+    })
+    https.get(url2,(res)=>{
         if(res.statusCode === 200){
             console.log('Server Restarted');
         }
